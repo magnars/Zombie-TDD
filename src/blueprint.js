9204b93f924d5187bf2f9fc8b399f4b5344f606f
Magnar Sveen <magnars@gmail.com>
2011-06-10 06:29:12 +0200

    off-screen linting

--- src/blueprint.js
+++ src/blueprint.js
@@ -14,16 +14,16 @@ var ZOMBIE = this.ZOMBIE || {};
   }
   
   function rotate() {
-    var shape = [];
-    for (var i = 0, l = this.getWidth(); i < l; i++) {
+    var shape = [], i, l;
+    for (i = 0, l = this.getWidth(); i < l; i += 1) {
       shape.push(this.getColumn(i).join(""));
     }
     this.shape = shape;
   }
   
   function getColumn(index) {
-    var column = [];
-    for (var i = 0, l = this.shape.length; i < l; i++) {
+    var column = [], i, l;
+    for (i = 0, l = this.shape.length; i < l; i += 1) {
       column.push(this.shape[i].charAt(index));
     }
     return column;
