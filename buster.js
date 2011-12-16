910e12445689a3e4a4ce9e3c0071927ad108df7c
Magnar Sveen <magnars@gmail.com>
2011-12-16 08:34:39 +0100

    Endret til dash for deling av ord i filer.  - ikke bare jåleri, også raskere å skrive pga shift-tasten.

--- buster.js
+++ buster.js
@@ -1,13 +1,13 @@
 var config = module.exports;
 
 config["Server tests"] = {
-  tests: ["test/shared/**/*_test.js"],
+  tests: ["test/shared/**/*-test.js"],
   environment: "node"
 };
 
 config["Browser tests"] = {
   libs: ["client_dependencies/**/*.js"],
   sources: ["lib/shared/**/*.js", "lib/client/**/*.js"],
-  tests: ["test/shared/**/*_test.js", "test/client/**/*_test.js"],
+  tests: ["test/shared/**/*-test.js", "test/client/**/*-test.js"],
   environment: "browser"
 };
--- test/shared/building-renderer-test.js
+++ test/shared/building-renderer-test.js
@@ -1,6 +1,6 @@
 if (typeof require === "function" && typeof module !== "undefined") {
   buster = require("buster");
-  ZOMBIE = { renderBuilding: require("../../lib/shared/building_renderer") };
+  ZOMBIE = { renderBuilding: require("../../lib/shared/building-renderer") };
 }
 
 (function (Z) {
