1ac50264843eb6601a995d38ae53f8ce5da381ea
Magnar Sveen <magnars@gmail.com>
2012-06-15 00:46:17 +0200

    Server listens to building events too.

--- app.js
+++ app.js
@@ -34,15 +34,30 @@ app.configure('production', function(){
 var bayeux = new faye.NodeAdapter({ mount: '/faye', timeout: 45 });
 var renderBuilding = require('lib/shared/building-renderer');
 
+var Z = {
+  building: require("lib/shared/building"),
+  eventHub: require("lib/shared/event-hub"),
+  buildingController: require("lib/shared/building-controller")
+};
+
+var building = Z.building.create({
+  zombies: 50,
+  barricade: 7,
+  rooms: [ { name: 'Trapdoor' } ],
+  sleepers: 4
+});
+
+var hub = Z.eventHub.create(bayeux.getClient());
+
+var controller = Z.buildingController.create({
+  building: building,
+  hub: hub
+}).init();
+
 app.get('/', function(req, res){
   res.render('index', {
     title: 'Zombie TDD',
-    buildingHTML: renderBuilding({
-      zombies: 50,
-      barricade: 7,
-      rooms: [ { name: 'Trapdoor' } ],
-      sleepers: 4
-    })
+    buildingHTML: renderBuilding(building)
   });
 });
 
--- test/integration/building-page-test.js
+++ test/integration/building-page-test.js
@@ -26,6 +26,17 @@
       setTimeout(done(function () {
         assert.equals(numRooms(), old + 1);
       }), 300);
+    },
+
+    "rooms aren't volatile": function (done) {
+      $(".buildRoom:first").trigger("click");
+
+      setTimeout(function () {
+        testHelper.getPageContents("/", done(function (html) {
+          var room = $(html).find(".room:last").text();
+          assert.equals(room, "Traphole");
+        }));
+      }, 300);
     }
   });
 }());
